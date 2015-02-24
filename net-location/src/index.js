8e8e49a07048d2a10016a56bb4f3c318f36938ac
asteere <awsteere@aol.com>
2015-02-23 18:58:57 -0700

    send server's ip address and port back in response

--- net-location/src/index.js
+++ net-location/src/index.js
@@ -6,8 +6,9 @@ var PORT = 8080;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('NODEJS: Hello world\n');
+  res.send('NODEJS: Hello world from IP: ' + req.socket.localAddress + ':' + PORT + '\n');
+  console.log("Responded to request from " + req);
 });
 
 app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+console.log(new Date() + ' Running on http://TBD:' + PORT);
