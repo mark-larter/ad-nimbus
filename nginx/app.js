5f76ac63734313422b65efacdf2beadade26b1df
Mark Larter <mark@freeheelgroup.com>
2015-03-05 19:58:42 -0700

    Merge branch 'master' into develop
    Conflicts:
    	config.rb
    	netlocation@.service
    	nginx/Dockerfile
    	nginx/app.js
    	user-data

--- nginx/app.js
+++ nginx/app.js
@@ -2,7 +2,11 @@
 var app = express();
 
 app.route('*').all(function(req, res) {
+<<<<<<< HEAD
   res.send('Hello World!');
+=======
+  res.send('NGINX: Hello World!');
+>>>>>>> master
 });
 
 app.listen(2000)
