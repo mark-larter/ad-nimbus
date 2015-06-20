33eb89ebceeffa5623b9dd1d3f8a0ffea4706441
Mark Larter <mark@freeheelgroup.com>
2015-06-19 22:21:17 -0600

    Add a few command line reminders.

--- netLocation/golang/docker/build/run-build-container.sh
+++ netLocation/golang/docker/build/run-build-container.sh
@@ -1,1 +1,6 @@
+# run the build container.
 docker run --name go-build -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker) -v `pwd`:/home/share -ti marklarter/go-build
+
+# run the go app.
+docker run -d -p=49160:8080 --name helloWorld marklarter/helloworld-go
+docker run -d -p=49161:8080 --name netLocation marklarter/netlocation-go
